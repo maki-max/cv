3ecd0ae9f0ff53cf3b59922ad20a6d8001afaa3b
maki-max <137396797+maki-max@users.noreply.github.com>
2023-09-20 13:12:45 +0300

    Add initial cv website

---
+++ flowers/index.js
@@ -0,0 +1,9 @@
+var numberOfDrumButtons = document.querySelectorAll(".btn").length;
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".btn")[0].addEventListener("click", result)
+  
+  function result(){
+    document.body.innerHTML="";
+  }
+   
+}
